d1ffa1d68b1c4e00f2700a403f9a6af6c012519a
Dushusir <1414556676@qq.com>
2023-03-29 15:16:30 +0800

    update

--- src/demo/Page.tsx
+++ src/demo/Page.tsx
@@ -13,6 +13,10 @@ export class Page extends React.Component<{style: {}}> {
         }
     }
 
+    componentDidUpdate(prevProps: Readonly<{ style: {}; }>, prevState: Readonly<{}>, snapshot?: any): void {
+        this.iframeRef.current && this.iframeRef.current.focus()
+    }
+
     render() {
         return <iframe ref={this.iframeRef} style={{...{width: '100%', height: '90%'}, ...this.props.style}}/>;
     }
