291fa07bdb44f5a6970a20a364c86005b6fa9774
Dushusir <1414556676@qq.com>
2023-08-14 15:02:15 +0800

    n

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // @ts-ignore
-  base: process.env.NODE_ENV === 'development' ? '' : './',
+  base: process.env.NODE_ENV === 'development' ? '' : '.
   plugins: [react()],
 })
